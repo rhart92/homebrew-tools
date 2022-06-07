32f42d076ee3d34bb8ae90e06ac15642c3155545
Ryan Hart <ryan@Ryans-MacBook-Pro.local>
2022-06-07 16:06:37 -0700

    Add cleanshot.

---
+++ Casks/clean-shot.rb
@@ -0,0 +1,12 @@
+cask "cleanshot" do
+  version "4"
+  url "https://updates.getcleanshot.com/v3/CleanShot-X-4.1.dmg"
+  name "Clean Shot X"
+  desc "Screenshot Software"
+  homepage "https://cleanshot.com"
+
+  auto_updates false
+  depends_on macos: ">= :el_capitan"
+
+  app "Clean Shot X.app"
+end
