1609e09d75670558642aa4599ac06738419fd074
Ryan Hart <ryan@makenotion.com>
2022-06-09 09:43:11 -0700

    add checksum

--- Casks/clean-shot.rb
+++ Casks/clean-shot.rb
@@ -1,5 +1,6 @@
 cask "clean-shot" do
   version "4"
+  sha256 "50f7926fa620cd89db57c38785de7e25883144583903c30de32451c5d11c4b25"
   url "https://updates.getcleanshot.com/v3/CleanShot-X-4.1.dmg"
   name "Clean Shot X"
   desc "Screenshot Software"
